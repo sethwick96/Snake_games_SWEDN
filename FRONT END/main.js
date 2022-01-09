630b5fdff5813c15e1359908325046c624b57a7c
sethwick96 <seth@sethwicker.com>
2022-01-08 21:00:34 -0500

    final update

--- FRONT END/main.js
+++ FRONT END/main.js
@@ -3,16 +3,13 @@ const snake_col = 'black';
 const snake_border = 'white';
 
 let snake = [
-    {x: 200, y: 200}
+    {x: 10, y: 10}
 ]
 
 //import board
-function Init() {
-    const snakeboard = document.getElementById("gameBoard");
+    const snakeboard = document.getElementById("gameCanvas");
     //make the board a 2d canvas
     const snakeboard_ctx = snakeboard.getContext("2d");
-}
-
 //start game
 Main();
 
@@ -21,27 +18,6 @@ function Main() {
 }
 
 function drawSnake() {
-    snake.forEach(drawSnakePart)
+    
 }
-function drawSnakePart(snakePart){
-    snakeboard_ctx.fillStyle = snake_col;
-    snakeboard_ctx.strokefill = snake_border;
-    snakeboard_ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
-    // Draw a border around the snake part
-    snakeboard_ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
-
-}
-  
-  //import the snake food
-
-
-
-
-
-
-
-
-
-
-window.onload = Init;
 console.log("This Works!");
