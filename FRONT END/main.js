2ea5c86ab23bb10966ff313eb4e526f4528b524c
Seth Wicker <seth@sethwicker.com>
2022-01-09 08:02:24 -0500

    late night work

--- FRONT END/main.js
+++ FRONT END/main.js
@@ -1,23 +1,34 @@
-//Snake Items
-const snake_col = 'black';
-const snake_border = 'white';
-
-let snake = [
-    {x: 10, y: 10}
-]
-
 //import board
-    const snakeboard = document.getElementById("gameCanvas");
+    let snakeboard = document.getElementById("gameCanvas");
     //make the board a 2d canvas
-    const snakeboard_ctx = snakeboard.getContext("2d");
-//start game
-Main();
+    let snakeboard_ctx = snakeboard.getContext("2d");
+    //set x and y for snakeboard_ctx
+    let snake_x = 100
+    let snake_y = 50
+    //set image variable
+    let img = document.getElementById("mc");
+    //set start button
+    let start = document.getElementById("button");
 
-function Main() {
-    drawSnake();
+function startGame() {
+    clearBoard();
+    drawSnake();  
+    moveSnake();
 }
+//clear the board
+function clearBoard(){
+    snakeboard_ctx.clearRect(0, 0, snakeboard.width, snakeboard.height);
+}
+//draw the snake on the canvas
+function drawSnake(){
+    snakeboard_ctx.drawImage(img,snake_x,snake_y,40,20);
+}
+//move the snake
+function moveSnake(){
+}
+startGame()
+//start game using start button
+start.addEventListener("click", startGame);
 
-function drawSnake() {
-    
-}
+
 console.log("This Works!");
